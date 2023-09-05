5a786d1a20c7c5efa73c776ddbffcd81e76a2160
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-09-05 11:22:42 -0700

    task6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for first_num in range(9):
+    for second_num in range(first_num + 1, 10):
+        if first_num == 8 and second_num == 9:
+            print("89\n")
+        else:
+            print("{:02d}, ".format(first_num * 10 + second_num), end='')
