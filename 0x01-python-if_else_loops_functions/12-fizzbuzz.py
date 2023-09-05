850a1a09bb4d54ddda511b9cd29502abad02477d
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-09-05 16:00:20 -0700

    task12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for number in range(1, 101):
+        result = ""
+        if number % 3 == 0 and number % 5 == 0:
+            result += "FizzBuzz"
+        elif number % 3 == 0:
+            result += "Fizz"
+        elif number % 5 == 0:
+            result += "Buzz"
+        else:
+            result = number
+        print("{}".format(result), end=' ')
+
