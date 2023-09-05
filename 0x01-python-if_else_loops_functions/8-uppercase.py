500f32e5638894694ddacdee809858c909de4ba0
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-09-05 14:52:28 -0700

    task8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(s):
+    for  letter in s:
+        if 'a' <= letter <= 'z':
+            up_letter = chr(ord(letter) - 32)
+            print(up_letter, end='')
+        else:
+            print(letter, end='')
+    print()
