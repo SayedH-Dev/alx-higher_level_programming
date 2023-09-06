b9f7908ec413932a0162bb8e6fad7e30ca0b1c86
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-09-06 07:22:20 -0700

    list-cycle-check

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+  * check_cycle - function that check for cycles within a singly linked list
+  * @list: pointer to the head of the linked list
+  * Return: 0 if no cycle or 1 if there is a cycle
+  */
+int check_cycle(listint_t *list)
+{
+	if (list == NULL || list->nNode == NULL)
+		return (0);
+
+	listint_t *tort = list;
+	listint_t *hare = list->nNode;
+
+	while (hare != NULL && hare->next != NULL)
+	{
+		if (tort == hare)
+			return (1);
+
+		tort = tort->nNode;
+		hare = hare->nNode->nNode;
+	}
+	return (0);
+}
---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,25 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/*libraries*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/*singly linked list node definition*/
+/**
+  * struct listint_t - singly linked list
+  * @x: data stored in the node with type integer
+  * @nNode: pointer to the next node
+  */
+struct listint_t
+{
+	int x;
+	struct listint_t *nNode;
+};
+
+typedef struct listint_t listint_t
+
+/*function declaration*/
+int check_cycle(listint_t *list);
+
+#endif
