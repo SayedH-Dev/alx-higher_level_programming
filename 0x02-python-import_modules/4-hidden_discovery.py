ed1666dba6c444fbba7b81254b38669a89be2123
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-09-07 12:35:03 -0700

    task4-bugfix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import marshal
-    with open("hidden_4.pyc", "rb") as pyc:
-        compiled = marshal.load(pyc)
-    names = (name for name in dir(compiled) if not name.startswith("__"))
+    import hidden_4
+    names = (name for name in dir(hidden_4) if not name.startswith("__"))
     for name in sorted(names):
         print(name)
