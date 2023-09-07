c744ee6036c6662f50dfb03d975dd070e9279ae5
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-09-07 10:47:01 -0700

    task4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,9 @@
 if __name__ == "__main__":
     import py_compile
     import imp
-    py_compile.compile("hidden_4.py")
-    compiled = imp.load_compiled("hidden_4", "hidden_4.pyc")
-    names = (name for name in dir(compiled) if not name.startswitch("__"))
+    hidden_4 = "hidden_4.pyc"
+    py_compile.compile(hidden_4)
+    compiled = imp.load_compiled("hidden_4", hidden_4)
+    names = (name for name in dir(compiled) if not name.startswith("__"))
     for name in sorted(names):
         print(name)
