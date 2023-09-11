7899d66d159fe18363700789bed84f8d50c2b5a8
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-09-11 06:18:08 -0700

    task6bf

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 def print_matrix_integer(matrix=[[]]):
     for array in matrix:
+        if not array:
+            print()
         for i in array:
             print("{:d}".format(i), end=" ")
         print()
