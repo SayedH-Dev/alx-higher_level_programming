7971c99f6ae493dec54d27dbc853b4f829a19796
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-09-13 04:50:39 -0700

    task7bf2

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-        upd_dictionary = dict(a_dictionary)
-        if type(key) is str:
-            upd_dictionary[key] = value
-        return upd_dictionary
+        a_dictionary[key] = value
+        return a_dictionary
