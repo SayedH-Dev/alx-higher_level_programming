c53b7e33cc7375e33cf970daca7378212158435b
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-09-26 07:26:12 -0700

    task0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+""" Square class definition """
+class Square:
+    """ this class represents a square """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+Readme file for project 0x06-python-classes at ALX SE
