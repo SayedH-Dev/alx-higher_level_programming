dc47c8ec1bd647b95b2d3c3ffe217086c06349c4
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-10-10 09:25:41 -0700

    task13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" module with a function to append a line of text after specific lines """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ function that append a line of text
+    after each line that contains a specific string
+    Args:
+        - str filename: the name of the required file
+        - str search_string: the string to search for in each line
+        - str new_string: the line of text to be inserted
+    """
+    with open(filename, 'r+') as file:
+        read_lines = file.readlines()
+        file.seek(0)
+
+        for line in read_lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string + '\n')
+        file.truncate()
