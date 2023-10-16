e51c341479e7925cbb1683b97b6918dca26b050c
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-10-16 08:29:29 -0700

    project

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ base class """
+import json
 
 
 class Base:
     __nb_objects = 0
+
     def __init__(self, id=None):
         """ constructor method """
         if id is not None:
@@ -11,3 +13,49 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ return JSON string representation of list_dictionaries """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ writes JSON string representation of list_objs to a file """
+        f = cls.__name__ + ".json"
+        dicts = [obj.to_dictionary() for obj in list_objs]
+        with open(f, mode='w', encoding='utf-8') as file:
+            file.write(cls.to_json_string(dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ returns a list of dictionaries from JSON string """
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ creates instance with attributes from a dictionary """
+        dummy = cls(1, 1) if cls.__name__ == "Rectangle" else cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """ returns instances list from JSON file """
+        f = cls.__name__ + ".json"
+        instance_list = []
+
+        try:
+            with open(f, mode='r', encoding='utf-8') as file:
+                json_string = file.read()
+                dictionaries = cls.from_json_string(json_string)
+                for dictionary in dictionaries:
+                    instance = cls.create(**dictionary)
+                    instance_list.append(instance)
+        except FileNotFoundError:
+            pass
+        return instance_list
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python3
+""" rectangle class """
+from models.base import Base
+
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ constructor method """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def height(self):
+        """ getter for height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ setter for height """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        """ getter for width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ setter for width """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def x(self):
+        """ getter for x """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ setter for x """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """ getter for y """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ setter for y """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """ return rectangle area """
+        return self.__width * self.__height
+
+    def display(self):
+        """ display rectangle with handling x & y"""
+        for i in range(self.__y):
+            print()
+        for j in range(self.__height):
+            print(' ' * self.__x + '#' * self.__width)
+
+    def __str__(self):
+        """ string representation of rectangle """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id,
+                self.__x,
+                self.__y,
+                self.__width,
+                self.__height)
+
+    def update(self, *args, **kwargs):
+        """ attributes update with arguments or keyworded arguments"""
+        if args:
+            attrs = ["id", "width", "height", "x", "y"]
+            for i in range(len(args)):
+                setattr(self, attrs[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ return dictionary of Rectangle """
+        return {
+                'id': self.id,
+                'width': self.width,
+                'height': self.height,
+                'x': self.x,
+                'y': self.y}
