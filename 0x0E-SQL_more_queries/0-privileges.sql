eaf400762b1ca405df21cc1b6a5a3f8ca9d82d44
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-11-15 03:21:47 -0800

    construction+task0

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- SQL query to list user privilages
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+Readme file for project 0x0E. SQL - More queries at ALX SE
