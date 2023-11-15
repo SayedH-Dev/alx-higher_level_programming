2ae118c227dc45915f2c69ba8d17a210419855fb
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-11-15 05:59:05 -0800

    task16bf

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,4 @@
 -- SQL query to list all shows and all the genres linked to it
--- on hbtn_0d_tvshows database
 SELECT tv_shows.title, tv_genres.name AS genre
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
